459b3f89a4003bb4df7c73403462480849df3b9d
Thushankavinda <thushankavinda95@gmail.com>
2021-10-21 10:28:29 +0530

    Data set database and passing to front end side

---
+++ src/main/java/com/example/springbootbackend/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.example.springbootbackend.controller;
+
+import com.example.springbootbackend.model.Employee;
+import com.example.springbootbackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping ("/api/v1")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //Get All Employees
+    @GetMapping("/employees")
+    public  List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+}
